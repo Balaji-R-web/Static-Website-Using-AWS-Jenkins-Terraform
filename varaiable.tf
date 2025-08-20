45e4a82b9db8bedf7d15c6f8f4ed58b74d68dc56
Balaji-R-web <balajir182001@gmail.com>
2025-08-20 14:53:02 +0530

    Delete varaiable.tf

--- varaiable.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  type        = string
-  description = "AWS region"
-}
-
-variable "bucket_name" {
-  type        = string
-  description = "Globally unique S3 bucket name"
-}
