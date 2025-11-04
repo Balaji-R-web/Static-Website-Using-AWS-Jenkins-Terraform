74d215149096a49e954285afd883affb86597699
Balaji-R-web <balajir182001@gmail.com>
2025-11-04 17:20:24 +0530

    Enhance S3 bucket configuration with public access
    Added public access block and updated resource definitions.

--- main.tf
+++ main.tf
@@ -15,9 +15,19 @@ resource "aws_s3_bucket" "static_site" {
     Name = "StaticWebsiteBucket"
   }
 }
+# Allow Terraform to apply a public bucket policy
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 resource "aws_s3_bucket_policy" "policy" {
   bucket = aws_s3_bucket.static_site.id
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -27,18 +37,20 @@ resource "aws_s3_bucket_policy" "policy" {
         Effect    = "Allow"
         Principal = "*"
         Action    = ["s3:GetObject"]
-        Resource  = ["${aws_s3_bucket.static_site.arn}/*"]
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
       }
     ]
   })
 }
 
-resource "aws_s3_bucket_object" "website_files" {
-  for_each = fileset("${path.module}/website", "*")
+
+
+resource "aws_s3_object" "website_files" {
+  for_each = fileset("${path.module}/website", "**/*")
 
   bucket       = aws_s3_bucket.static_site.bucket
   key          = each.value
   source       = "${path.module}/website/${each.value}"
-  etag         = filemd5("${path.module}/website/${each.value}")
   content_type = "text/html"
+  etag         = filemd5("${path.module}/website/${each.value}")
 }
